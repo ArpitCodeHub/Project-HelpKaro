6e98e3d49b6616aa8392d2cc069f1e7e1d4acdd0
nishthaasood <soodnishtha12462@gmail.com>
2025-03-18 21:51:56 +0530

    contact-me-page

---
+++ index.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const faqQuestions = document.querySelectorAll('.faq-question');
+    
+    faqQuestions.forEach(question => {
+      question.addEventListener('click', () => {
+        
+        question.classList.toggle('active');
+        
+        const answer = question.nextElementSibling;
+       
+        answer.classList.toggle('active');
+      });
+    });
+});
