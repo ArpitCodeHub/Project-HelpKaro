efc0c73bf5b0cf7493e255be7a3000a9c2f0022b
Aakriti Arya <aakritiarya2005@gmail.com>
2025-03-19 23:31:32 +0530

    Updated Navbar and footer in contact us page

--- contact-me/contact.js
+++ contact-me/contact.js
@@ -1,4 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
+  async function loadComponent(url, elementId) {  
+    const res = await fetch(url);                     // Fetches the content of the URL
+    const content = await res.text();                 // Converts the response into text
+    document.getElementById(elementId).innerHTML = content;  // Injects the content into the specified element
+}
+
+// Load navbar and footer
+loadComponent("navbar.html", "navbar");        // Loads the navbar into the element with id="navbar"
+loadComponent("footer.html", "footer");
+
+
     const faqQuestions = document.querySelectorAll('.faq-question');
     
     faqQuestions.forEach(question => {
